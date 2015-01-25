97b41bf9ca09e03d603477d54acf5d30b4ba9322
Jim Kirkbride <james.kirkbride@gmail.com>
2015-01-25 08:26:53 -0500

    Initial commit.  Robolectric configured.

---
+++ app/src/androidTest/java/com/groceryreminder/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.groceryreminder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void shouldExecute() {
+        assertTrue(true);
+    }
+}
