1c50d1fd563cc4ede040df9cd641ee81644795ac
Jim Kirkbride <james.kirkbride@gmail.com>
2015-01-31 14:32:51 -0500

    Espresso tests running, but colliding with Robolectric tests.

---
+++ app/src/androidTest/java/com/groceryreminder/espresso/MainActivityEspressoTest.java
@@ -0,0 +1,29 @@
+package com.groceryreminder.espresso;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.groceryreminder.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+public class MainActivityEspressoTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    public MainActivityEspressoTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testHelloWorld() {
+        onView(withText("Hello world!")).check(ViewAssertions.matches(isDisplayed()));
+    }
+}
