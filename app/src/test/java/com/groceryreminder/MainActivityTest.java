95839ef8dd7c1129d3f5f35eb9d7efff9d2b8503
Jim Kirkbride <james.kirkbride@gmail.com>
2015-01-31 17:17:55 -0500

    Adding the reminders list and reminder edit text.  First simple test passing.

--- app/src/test/java/com/groceryreminder/MainActivityTest.java
+++ app/src/test/java/com/groceryreminder/MainActivityTest.java
@@ -1,16 +1,31 @@
 package com.groceryreminder;
 
+import android.widget.ListView;
+
+import com.groceryreminder.MainActivity;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
+    private MainActivity activity;
+
+    @Before
+    public void setUp() {
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
+    }
+
     @Test
-    public void shouldExecute() {
-        assertTrue(true);
+    public void givenNoRemindersExistThenTheReminderListIsEmpty() {
+        ListView listView = (ListView)activity.findViewById(R.id.reminders_list);
+        assertEquals(listView.getCount(), 0);
     }
 }
