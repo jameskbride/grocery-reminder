ddb1a285a24693676bbee46b15c0c434448aeaad
Jim Kirkbride <jkirkbride@pillartechnology.com>
2015-02-15 15:20:54 -0500

    Fixed the Espresso test.

--- app/src/androidTest/java/com/groceryreminder/espresso/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/groceryreminder/espresso/MainActivityEspressoTest.java
@@ -4,10 +4,14 @@ import android.support.test.espresso.assertion.ViewAssertions;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import com.groceryreminder.R;
 import com.groceryreminder.views.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
@@ -23,7 +27,10 @@ public class MainActivityEspressoTest extends ActivityInstrumentationTestCase2<M
         getActivity();
     }
 
-    public void testHelloWorld() {
-        onView(withText("Hello world!")).check(ViewAssertions.matches(isDisplayed()));
+    public void testWhenAReminderIsAddedThenIsIsDisplayedInTheList() {
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.add_reminder_edit)).perform(typeText("test"));
+        onView(withId(R.id.add_reminder_button)).perform(click());
+        onView(withText("test")).check(ViewAssertions.matches(isDisplayed()));
     }
 }
