bb76063e13f7dbf7089de65027be57662316c189
Jim Kirkbride <james.kirkbride@gmail.com>
2015-03-16 19:53:16 -0400

    Persisting places.

--- app/src/main/java/com/groceryreminder/services/GroceryLocatorService.java
+++ app/src/main/java/com/groceryreminder/services/GroceryLocatorService.java
@@ -1,16 +1,25 @@
 package com.groceryreminder.services;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
+import android.util.Log;
 
+import com.groceryreminder.data.ReminderContract;
 import com.groceryreminder.injection.ReminderApplication;
+import com.groceryreminder.models.Reminder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import se.walkercrou.places.GooglePlacesInterface;
 import se.walkercrou.places.Param;
+import se.walkercrou.places.Place;
 import se.walkercrou.places.Types;
 
 public class GroceryLocatorService extends IntentService {
@@ -33,7 +42,20 @@ public class GroceryLocatorService extends IntentService {
         Location location = locationManager.getLastKnownLocation("provider");
 
         Param groceryStoreType = Param.name(GooglePlacesInterface.STRING_TYPE).value(Types.TYPE_GROCERY_OR_SUPERMARKET);
-        Param[] params = new Param[] {groceryStoreType};
-        googlePlaces.getPlacesByRadar(location.getLatitude(), location.getLongitude(), FIVE_MILES_IN_METERS, 50, params);
+        List<Place> places = googlePlaces.getPlacesByRadar(location.getLatitude(), location.getLongitude(), FIVE_MILES_IN_METERS, 50, groceryStoreType);
+
+        List<ContentValues> contentValuesList = new ArrayList<ContentValues>();
+
+        for (Place place : places) {
+            Log.d("GroceryLocatorService", "Found places");
+            ContentValues values = new ContentValues();
+            values.put(ReminderContract.Locations.NAME, place.getName());
+            values.put(ReminderContract.Locations.PLACES_ID, place.getPlaceId());
+            values.put(ReminderContract.Locations.LATITUDE, place.getLatitude());
+            values.put(ReminderContract.Locations.LONGITUDE, place.getLongitude());
+            contentValuesList.add(values);
+        }
+
+        getContentResolver().bulkInsert(ReminderContract.Locations.CONTENT_URI, contentValuesList.toArray(new ContentValues[contentValuesList.size()]));
     }
 }
--- app/src/test/java/com/groceryreminder/services/GroceryLocatorServiceTest.java
+++ app/src/test/java/com/groceryreminder/services/GroceryLocatorServiceTest.java
@@ -1,30 +1,49 @@
 package com.groceryreminder.services;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 
 import com.groceryreminder.RobolectricTestBase;
+import com.groceryreminder.data.ReminderContentProvider;
+import com.groceryreminder.data.ReminderContract;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowContentProvider;
+import org.robolectric.shadows.ShadowContentResolver;
 import org.robolectric.shadows.ShadowLocationManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import se.walkercrou.places.GooglePlacesInterface;
 import se.walkercrou.places.Param;
+import se.walkercrou.places.Place;
 import se.walkercrou.places.Types;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyDouble;
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyVararg;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
@@ -36,9 +55,12 @@ public class GroceryLocatorServiceTest extends RobolectricTestBase {
     private LocationManager locationManager;
     private ShadowLocationManager shadowLocationManager;
     private Location defaultLastKnownLocation;
+    private ReminderContentProvider reminderProvider;
+    private ShadowContentResolver shadowContentResolver;
 
     @Before
     public void setUp() {
+        super.setUp();
         groceryLocatorService = new GroceryLocatorService(ARBITRARY_SERVICE_NAME);
         groceryLocatorService.onCreate();
 
@@ -47,6 +69,11 @@ public class GroceryLocatorServiceTest extends RobolectricTestBase {
         this.shadowLocationManager = Robolectric.shadowOf(locationManager);
         this.defaultLastKnownLocation = createDefaultLocation();
         shadowLocationManager.setLastKnownLocation("provider", defaultLastKnownLocation);
+        reminderProvider = new ReminderContentProvider();
+        reminderProvider.onCreate();
+        shadowContentResolver = Robolectric.shadowOf(groceryLocatorService.getContentResolver());
+        shadowContentResolver.registerProvider(ReminderContract.AUTHORITY, reminderProvider);
+
     }
 
     @Test
@@ -81,6 +108,32 @@ public class GroceryLocatorServiceTest extends RobolectricTestBase {
         groceryLocatorService.onHandleIntent(new Intent());
 
         verify(googlePlacesMock).getPlacesByRadar(eq(defaultLastKnownLocation.getLatitude()), eq(defaultLastKnownLocation.getLongitude()), anyDouble(), anyInt(), any(Param[].class));
+    }
+
+    @Test
+    public void whenPlaceSearchResultsAreFoundThenTheyArePersisted() {
+        Place place = createDefaultGooglePlace();
+
+        List<Place> places = new ArrayList<Place>();
+        places.add(place);
+
+        Param groceryStoreType = Param.name(GooglePlacesInterface.STRING_TYPE).value(Types.TYPE_GROCERY_OR_SUPERMARKET);
+        doReturn(places).when(googlePlacesMock).getPlacesByRadar(defaultLastKnownLocation.getLatitude(), defaultLastKnownLocation.getLongitude(), GroceryLocatorService.FIVE_MILES_IN_METERS, 50, groceryStoreType);
+
+        groceryLocatorService.onHandleIntent(new Intent());
+        verify(googlePlacesMock).getPlacesByRadar(anyDouble(), anyDouble(), anyDouble(), anyInt(), (Param[])anyVararg());
+
+        Cursor cursor = reminderProvider.query(ReminderContract.Locations.CONTENT_URI, ReminderContract.Locations.PROJECT_ALL, "", null, null);
+        assertEquals(1, cursor.getCount());
+    }
+
+    private Place createDefaultGooglePlace() {
+        Place place = new Place();
+        place.setName("test");
+        place.setLatitude(0.0);
+        place.setLongitude(1.1);
+        place.setPlaceId("test_id");
+        return place;
     }
 
     private Location createDefaultLocation() {
