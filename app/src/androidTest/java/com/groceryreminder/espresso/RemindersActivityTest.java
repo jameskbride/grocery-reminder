a74b6fd5ae3ca3e57533f58df733843637f77256
Jim Kirkbride <james.kirkbride@gmail.com>
2015-04-20 08:49:09 -0400

    Test fails with ClassCastException on Application.

--- app/src/androidTest/java/com/groceryreminder/espresso/RemindersActivityTest.java
+++ app/src/androidTest/java/com/groceryreminder/espresso/RemindersActivityTest.java
@@ -8,6 +8,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.groceryreminder.R;
+import com.groceryreminder.injection.EspressoReminderApplication;
 import com.groceryreminder.views.reminders.RemindersActivity;
 
 import org.junit.After;
@@ -16,6 +17,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+
+import se.walkercrou.places.GooglePlacesInterface;
+import se.walkercrou.places.Param;
+import se.walkercrou.places.Place;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -26,6 +33,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -89,6 +100,34 @@ public class RemindersActivityTest extends ActivityInstrumentationTestCase2<Remi
 
         onView(withText(getActivity().getApplication().getString(R.string.store_list_title)))
                 .check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void testWhenTheStoresActionBarButtonIsPressedThenTheGroceryStoresAreListed() {
+        EspressoReminderApplication app = (EspressoReminderApplication)getInstrumentation().getTargetContext().getApplicationContext();
+
+        List<Place> places = new ArrayList<Place>();
+        Place arbitraryPlace = createDefaultGooglePlace();
+        places.add(arbitraryPlace);
+        GooglePlacesInterface googlePlacesMock = app.getEspressoRemoteResourcesModule().getGooglePlaces();
+        when(googlePlacesMock.getNearbyPlacesRankedByDistance(anyDouble(), anyDouble(), (Param[]) anyVararg())).thenReturn(places);
+
+        onView(withId(R.id.action_find_stores)).perform(click());
+
+        onView(withText(getActivity().getApplication().getString(R.string.store_list_title)))
+                .check(ViewAssertions.matches(isDisplayed()));
+
+        verify(googlePlacesMock).getNearbyPlacesRankedByDistance(anyDouble(), anyDouble(), (Param[]) anyVararg());
+    }
+
+    private Place createDefaultGooglePlace() {
+        Place place = new Place();
+        place.setName("test");
+        place.setLatitude(0.0);
+        place.setLongitude(1.1);
+        place.setPlaceId("test_id");
+
+        return place;
     }
 
     private void addArbitraryReminder() {
