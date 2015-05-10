ffc19ff88eedf854e58841b31083e4214ac96718
Jim Kirkbride <james.kirkbride@gmail.com>
2015-05-10 15:26:22 -0400

    Driving in query.

--- app/src/main/java/com/groceryreminder/data/ReminderContentProvider.java
+++ app/src/main/java/com/groceryreminder/data/ReminderContentProvider.java
@@ -5,6 +5,7 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
 public class ReminderContentProvider  extends ContentProvider{
@@ -18,7 +19,15 @@ public class ReminderContentProvider  extends ContentProvider{
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        queryBuilder.setTables(DBSchema.REMINDERS);
+        Cursor cursor = queryBuilder.query(reminderDBHelper.getReadableDatabase(), projection, selection, selectionArgs, null, null, sortOrder);
+
+        //Cannot currently test-drive this line: minSdk must be 19, currently set to 15
+        cursor.setNotificationUri(getContext().getContentResolver(), ReminderContract.Locations.CONTENT_URI);
+
+        return cursor;
     }
 
     @Override
--- app/src/test/java/com/groceryreminder/data/ReminderContentProviderTest.java
+++ app/src/test/java/com/groceryreminder/data/ReminderContentProviderTest.java
@@ -2,6 +2,7 @@ package com.groceryreminder.data;
 
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 
 import com.groceryreminder.BuildConfig;
@@ -40,11 +41,27 @@ public class ReminderContentProviderTest extends RobolectricTestBase {
     @Test
     public void givenAReminderContentTypeAndContentValuesWhenARecordIsInsertedThenAURIWithRecordIDShouldBeReturned()
     {
-        ContentValues values = new ReminderValuesBuilder().createDefaultReminderValues().build();
+        ContentValues values = createDefaultReminderValues();
         Uri expectedUri = provider.insert(ReminderContract.Reminders.CONTENT_URI, values);
 
         assertNotNull(expectedUri);
         assertEquals(1, ContentUris.parseId(expectedUri));
     }
 
+    private ContentValues createDefaultReminderValues() {
+        return new ReminderValuesBuilder().createDefaultReminderValues().build();
+    }
+
+    @Test
+    public void whenADuplicateReminderIsInsertedThenItThereIsNoConflict() {
+        ContentValues initialValues = createDefaultReminderValues();
+        provider.insert(ReminderContract.Reminders.CONTENT_URI, initialValues);
+        ContentValues duplicateValues = createDefaultReminderValues();
+        duplicateValues.put(ReminderContract.Reminders.DESCRIPTION, initialValues.getAsString(ReminderContract.Reminders.DESCRIPTION));
+
+        Uri expectedUri = provider.insert(ReminderContract.Locations.CONTENT_URI, duplicateValues);
+
+        Cursor cursor = provider.query(expectedUri, ReminderContract.Reminders.PROJECT_ALL, "", null, null);
+        assertEquals(2, cursor.getCount());
+    }
 }
