6b2e24194a938c3ffff32f9d517b479ecec7326e
Jim Kirkbride <james.kirkbride@gmail.com>
2015-05-10 15:28:11 -0400

    Adding coverage for notify on insert.

--- app/src/test/java/com/groceryreminder/data/ReminderContentProviderTest.java
+++ app/src/test/java/com/groceryreminder/data/ReminderContentProviderTest.java
@@ -13,8 +13,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowContentResolver;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -63,5 +69,16 @@ public class ReminderContentProviderTest extends RobolectricTestBase {
 
         Cursor cursor = provider.query(expectedUri, ReminderContract.Reminders.PROJECT_ALL, "", null, null);
         assertEquals(2, cursor.getCount());
+    }
+
+    @Test
+    public void givenAReminderContentTypeAndContentValuesWhenARecordIsInsertedThenObserversAreNotified()
+    {
+        ContentValues values = createDefaultReminderValues();
+        ShadowContentResolver contentResolver = Shadows.shadowOf(provider.getContext().getContentResolver());
+        Uri expectedUri = provider.insert(ReminderContract.Reminders.CONTENT_URI, values);
+
+        List<ShadowContentResolver.NotifiedUri> notifiedUriList = contentResolver.getNotifiedUris();
+        assertThat(notifiedUriList.get(0).uri, is(expectedUri));
     }
 }
