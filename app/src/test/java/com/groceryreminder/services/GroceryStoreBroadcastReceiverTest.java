cfd7bf4135a335b2d4e63ae481cc9314e1c69267
Jim Kirkbride <james.kirkbride@gmail.com>
2015-07-13 07:52:57 -0400

    Added coverage for GroceryStoreBroadcastReceiver.  Ready to start filtering notifications by location accuracy.

--- app/src/test/java/com/groceryreminder/services/GroceryStoreBroadcastReceiverTest.java
+++ app/src/test/java/com/groceryreminder/services/GroceryStoreBroadcastReceiverTest.java
@@ -2,6 +2,7 @@ package com.groceryreminder.services;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.location.LocationManager;
 
 import com.groceryreminder.BuildConfig;
 import com.groceryreminder.RobolectricTestBase;
@@ -17,7 +18,12 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class)
@@ -36,6 +42,7 @@ public class GroceryStoreBroadcastReceiverTest extends RobolectricTestBase {
 
     private Intent buildIntentToListenFor() {
         Intent intentToListenFor =  new Intent(GroceryReminderConstants.ACTION_STORE_PROXIMITY_EVENT);
+        intentToListenFor.putExtra(ReminderContract.Locations.NAME, ARBITRARY_STORE_NAME);
 
         return intentToListenFor;
     }
@@ -48,5 +55,34 @@ public class GroceryStoreBroadcastReceiverTest extends RobolectricTestBase {
         broadcastReceiver.onReceive(RuntimeEnvironment.application, intent);
 
         verifyNoMoreInteractions(groceryStoreNotificationManagerMock);
+    }
+
+    @Test
+    public void givenAValidProximityAlertWhenTheNoticeCannotBeSentThenTheNoticeIsNotSent() {
+        Intent intent = buildIntentToListenFor();
+        intent.putExtra(LocationManager.KEY_PROXIMITY_ENTERING, true);
+
+        GroceryStoreNotificationManagerInterface groceryStoreNotificationManagerMock = getTestReminderModule().getGroceryStoreNotificationManager();
+
+        when(groceryStoreNotificationManagerMock.noticeCanBeSent(eq(ARBITRARY_STORE_NAME), anyLong())).thenReturn(false);
+        broadcastReceiver.onReceive(RuntimeEnvironment.application, intent);
+
+        verify(groceryStoreNotificationManagerMock).noticeCanBeSent(eq(ARBITRARY_STORE_NAME), anyLong());
+        verifyNoMoreInteractions(groceryStoreNotificationManagerMock);
+    }
+
+    @Test
+    public void givenAValidProximityAlertWhenTheNoticeCanBeSentThenTheNoticeIsSent() {
+        Intent intent = buildIntentToListenFor();
+        intent.putExtra(LocationManager.KEY_PROXIMITY_ENTERING, true);
+
+        GroceryStoreNotificationManagerInterface groceryStoreNotificationManagerMock = getTestReminderModule().getGroceryStoreNotificationManager();
+
+        when(groceryStoreNotificationManagerMock.noticeCanBeSent(eq(ARBITRARY_STORE_NAME), anyLong())).thenReturn(true);
+        broadcastReceiver.onReceive(RuntimeEnvironment.application, intent);
+
+        verify(groceryStoreNotificationManagerMock).noticeCanBeSent(eq(ARBITRARY_STORE_NAME), anyLong());
+        verify(groceryStoreNotificationManagerMock).sendNotification(intent);
+        verify(groceryStoreNotificationManagerMock).saveNoticeDetails(eq(ARBITRARY_STORE_NAME), anyLong());
     }
 }
