be470c6a66bd4f8dd9cff7027c9af38cbbcaa8c3
Jim Kirkbride <james.kirkbride@gmail.com>
2015-07-20 20:50:35 -0400

    Reducing network update time.

--- app/src/main/java/com/groceryreminder/domain/GroceryReminderConstants.java
+++ app/src/main/java/com/groceryreminder/domain/GroceryReminderConstants.java
@@ -7,7 +7,7 @@ public class GroceryReminderConstants {
     public static final String ACTION_STORE_PROXIMITY_EVENT = "com.groceryreminder.STORE_PROXIMITY_EVENT";
 
     public static final int NOTIFICATION_PROXIMITY_ALERT = 1;
-    public static final float LOCATION_GEOFENCE_RADIUS_METERS = 200f;
+    public static final float LOCATION_GEOFENCE_RADIUS_METERS = 500f;
     public static final int MAXIMUM_ACCURACY_IN_METERS = 100;
 
     public static final long[] PROXIMITY_VIBRATION_PATTERN = {1000, 1000};
@@ -18,6 +18,6 @@ public class GroceryReminderConstants {
     public static final String LAST_NOTIFICATION_TIME = "LAST_NOTIFICATION_TIME";
     public static final String LISTEN_FOR_GPS_EXTRA = "LISTEN_FOR_GPS_EXTRA";
     public static final String LAST_GOOGLE_PLACES_POLL_TIME = "LAST_GOOGLE_PLACES_POLL_TIME";
-    public static final int NETWORK_MIN_UPDATE_TIME = 60000;
+    public static final int NETWORK_MIN_UPDATE_TIME = 45000;
     public static final int PASSIVE_MIN_UPDATE_TIME = 0;
 }
